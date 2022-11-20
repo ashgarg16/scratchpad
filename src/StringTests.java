f958f64dce2e02be369b53ed20a50166c7f45466
ashgarg <ashgarg16@gmail.com>
2022-11-20 19:28:15 +0530

    string exercise

---
+++ src/StringTests.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class StringTests {
+
+    public static void main(String[] args) {
+
+        List<String> stringList = Arrays.asList("one", "two", "three", "four");
+                //List.of("one", "two", "three", "four");
+
+        //Inplace sorting
+        Collections.sort(stringList, (s1, s2)-> s1.length()- s2.length());
+        System.out.println(stringList);
+        //Inplace sorting in reverse order
+        Collections.sort(stringList, (s1,s2)->s2.length()-s1.length());
+        System.out.println(stringList);
+
+        // producing new sorted list
+        stringList.stream()
+                .sorted((s1, s2)-> s1.length()- s2.length())
+                .forEach(System.out::println);
+        System.out.println(stringList);
+    }
+}
